1929fa69fd50ddfb28124dedd9d6c6a8747765ec
syzik <camus.florian@gmail.com>
2019-06-13 14:47:49 +0200

    crash ?

--- pythonConnector.py
+++ pythonConnector.py
@@ -50,14 +50,15 @@ def readfile(filename, conn):
     global error
     with open(filename) as f:
         for line in f:
-            charesult = chardet.detect(line)
             try :
+                charesult = chardet.detect(line)
                 parse(line.decode(charesult['encoding']),conn)
             except :
                 error += 1
                 print("error : %s\n" % (error))
                 continue
             linecount = linecount + 1
+            print("line checked : %s\n"% linecount)
 
 def checkmailorlogin(world):
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
